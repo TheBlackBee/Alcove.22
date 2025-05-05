44ec760ea251550783c8a7d2652979c2cb2150bb
TheBlackBee <53703851+TheBlackBee@users.noreply.github.com>
2025-05-05 12:35:10 -0400

    Create Users.js
    to test user ids

---
+++ Users.js
@@ -0,0 +1,7 @@
+// users.js
+const users = [
+  { id: 'bracelet_001', name: 'Imara', avatar: '🧕', vitals: { heartRate: 78, temp: 36.5 } },
+  { id: 'bracelet_002', name: 'Warren', avatar: '👨🏾‍🦳', vitals: { heartRate: 82, temp: 36.7 } },
+  { id: 'bracelet_003', name: 'Nia', avatar: '👵🏿', vitals: { heartRate: 70, temp: 36.6 } }
+];
+let currentUser = users[0]; // default active user
